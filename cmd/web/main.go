b2d2e17da20cf8ff66069cc14ca21ca3916e1f88
Qasim Abdullah <qasim.abdullah@travis-ci.org>
2023-08-31 16:55:07 +0500

    add mysql db connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,10 +17,17 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP Network address")
+	dsn := flag.String("dsn", "snapbinuser:admin@/snapbin?parseTime=true", "MySQL DSN")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDBConnection(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -31,9 +41,22 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on port %s..", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func openDBConnection(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/qasim-at-tci/snapbin
 
 go 1.20
+
+require github.com/go-sql-driver/mysql v1.7.1
